1464d61783fe745364e30fc31450ed3ef10a67cb
webrian <webrian@users.noreply.github.com>
2023-11-29 08:26:18 +0100

    Add starts_with and ends_with expressions

---
+++ ends_with.py
@@ -0,0 +1,19 @@
+from qgis.core import *
+from qgis.gui import *
+
+@qgsfunction(args='auto', group='String', referenced_columns=[])
+def ends_with(input_string, compare_string, feature, parent):
+    """
+    Checks if the input string ends with the comparison string
+    <h4>Syntax</h4><br/>
+    <code>ends_with(input_string, compare_string)</code>
+
+    <h4>Arguments</h4><br/>
+    <code>input_string</code> - is string. The string.
+    <br>
+    <code>compare_string</code> - is string. The string to compare
+
+    <h4>Example</h4><br/>
+    <code>ends_with('Hello World', 'd') -> True</code>
+    """
+    return str(input_string).endswith(compare_string)
---
+++ starts_with.py
@@ -0,0 +1,19 @@
+from qgis.core import *
+from qgis.gui import *
+
+@qgsfunction(args='auto', group='String', referenced_columns=[])
+def starts_with(input_string, compare_string, feature, parent):
+    """
+    Checks if the input string starts with the comparison string
+    <h4>Syntax</h4><br/>
+    <code>starts_with(input_string, compare_string)</code>
+
+    <h4>Arguments</h4><br/>
+    <code>input_string</code> - is string. The string.
+    <br>
+    <code>compare_string</code> - is string. The string to compare.
+
+    <h4>Example</h4><br/>
+    <code>starts_with('Hello World', 'H') -> True</code>
+    """
+    return str(input_string).startswith(compare_string)
